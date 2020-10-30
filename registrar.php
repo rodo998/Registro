02dd92584fccb754657a53906b005f732f13d31c
rodo998 <56892071+rodo998@users.noreply.github.com>
2020-10-30 03:11:46 -0600

    Update registrar.php

--- registrar.php
+++ registrar.php
@@ -12,7 +12,7 @@ if (isset($_POST['register'])) {
 	    $name = trim($_POST['name']);
 	    $email = trim($_POST['email']);
 	    $fechareg = date("d/m/y");
-	    $consulta = "INSERT INTO datos(nombre, email, fecha_reg) VALUES ('$name','$email','$fechareg')";
+	    $consulta = "INSERT INTO directorio.persona(id, nombre, email, fecha_reg) VALUES (0,'$name','$email','$fechareg')";
 	    $resultado = mysqli_query($conex,$consulta);
 	    if ($resultado) {
 	    	?> 
