e6be3561361257267057a41bf1e61a4eb56822f9
rodo998 <56892071+rodo998@users.noreply.github.com>
2020-10-30 03:04:13 -0600

    Update registrar.php

--- registrar.php
+++ registrar.php
@@ -1,7 +1,9 @@
 <?php 
 
 include("con_db.php");
-
+if($conex){
+	echo "conexion establecida";
+}
 if (isset($_POST['register'])) {
     if (strlen($_POST['name']) >= 1 && strlen($_POST['email']) >= 1) {
 	    $name = trim($_POST['name']);
