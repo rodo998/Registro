869eba139d543fd4ee0ff361585e7dad35d5a696
rodo998 <rca.14@hotmail.com>
2020-10-30 19:31:03 -0600

    Update registrar.php

--- registrar.php
+++ registrar.php
@@ -3,29 +3,28 @@
 include("con_db.php");
 
 if($conex){
-	echo "¡Conexion con la base de datos esablecida!"
+	echo "conexion establecida"
 }
 
 if (isset($_POST['register'])) {
     if (strlen($_POST['name']) >= 1 && strlen($_POST['email']) >= 1) {
 	    $name = trim($_POST['name']);
-		$email = trim($_POST['email']);
-		$phone = trim($_POST['phone']);
+	    $email = trim($_POST['email']);
 	    $fechareg = date("d/m/y");
-	    $consulta = "INSERT INTO directorio.personas(nombre, email, telefono, fecha_registro) VALUES ('$name','$email','$phone','$fechareg')";
+	    $consulta = "INSERT INTO datos(nombre, email, fecha_reg) VALUES ('$name','$email','$fechareg')";
 	    $resultado = mysqli_query($conex,$consulta);
 	    if ($resultado) {
-		?> 
-	    	<h3 class="ok">¡Registrado Correctamente!</h3>
+	    	?> 
+	    	<h3 class="ok">¡Te has inscripto correctamente!</h3>
            <?php
 	    } else {
 	    	?> 
-	    	<h3 class="bad">¡Ha ocurrido un error!</h3>
+	    	<h3 class="bad">¡Ups ha ocurrido un error!</h3>
            <?php
 	    }
     }   else {
 	    	?> 
-	    	<h3 class="bad">¡Flatan campos por llenar!</h3>
+	    	<h3 class="bad">¡Por favor complete los campos!</h3>
            <?php
     }
 }
