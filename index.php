e0a23b60c65ccb2fe63956d4883c74384e3197e6
rodo998 <rca.14@hotmail.com>
2020-10-30 19:22:45 -0600

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,17 @@
+
 <!DOCTYPE html>
 <html>
 <head>
-	<title>Registrar Directorio</title>
+	<title>Registrar usuario</title>
 	<meta charset="utf-8">
 	<link rel="stylesheet" type="text/css" href="estilo.css">
 </head>
 <body>
     <form method="post">
-    	<h1>Sistema de Registro Rodolfo Cervantes</h1>
+    	<h1>¡Suscribete!</h1>
     	<input type="text" name="name" placeholder="Nombre completo">
     	<input type="email" name="email" placeholder="Email">
-		<input type="tel" name="phone" placeholder="ej. 8441232312">
-    	<input type="submit" name="register" value="Registrar">
+    	<input type="submit" name="register">
     </form>
         <?php 
         include("registrar.php");
