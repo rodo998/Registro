665dbee40455821a536b85a42533f2dc87f0053e
rodo998 <rca.14@hotmail.com>
2020-10-30 19:15:56 -0600

    error-update

--- registrar.php
+++ registrar.php
@@ -10,12 +10,12 @@ if (isset($_POST['register'])) {
     if (strlen($_POST['name']) >= 1 && strlen($_POST['email']) >= 1) {
 	    $name = trim($_POST['name']);
 		$email = trim($_POST['email']);
-		$phone = trim($_POST['phone'])
+		$phone = trim($_POST['phone']);
 	    $fechareg = date("d/m/y");
 	    $consulta = "INSERT INTO directorio.personas(nombre, email, telefono, fecha_registro) VALUES ('$name','$email','$phone','$fechareg')";
 	    $resultado = mysqli_query($conex,$consulta);
 	    if ($resultado) {
-	    	?> 
+		?> 
 	    	<h3 class="ok">¡Registrado Correctamente!</h3>
            <?php
 	    } else {
